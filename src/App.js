12f0584c9e2f8ce3f72805646d4077bfca38587a
Rebecca Mollica <rebecca.mollica@live.it>
2021-12-30 12:32:09 +0100

    components and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Tweet from './components/Tweet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tweet name='Rebecca' message='This is a random tweet' likes='22' />
+      <Tweet name='Pippo' message='A second tweet' likes='44' />
+      <Tweet name='Benny' message='A third tweet' likes='66' />
+      <Tweet name='Ercole' message='A fourth tweet' likes='88' />
     </div>
   );
 }
 
 export default App;
+
+// PRIMO STEP: creo un apposito file, 
+// in questo caso Tweet.jsx, che in seguito
+// importerò come componente in questa funzione.
+
+// TERZO STEP: dopo aver creato il componente, 
+// procedo ad importarlo in cima alla pagina,
+// indicandone la directory di provenienza.
+
+// P.S. Talvolta è sufficiente inserire il componente
+// direttamente all'interno della funzione,
+// VSCode provvederà a compilare ed inserire il suo
+// import in automatico.
+
+// QUARTO STEP:
+// In che modo posso creare multiple versioni DINAMICHE del mio componente?
+// Posso farlo attraverso le PROPS, ossia delle proprietà che posso definire
+// sulla base di ogni singolo componente.
+// Dal momento che ho aggiunto 'name' al componente Tweet, 
+// andrò ad inserire tale props come suo parametro in Tweet.jsx.
+// Lo stesso procedimento va seguito per modificare dinamicamente
+// gli altri elementi all'interno del componente.
---
+++ src/components/Tweet.jsx
@@ -0,0 +1,26 @@
+import styles from './Tweet.module.scss';
+
+function Tweet(props) {
+    return (
+        <div className={styles.tweet}>
+            <h3>{props.name}</h3>
+            <p>{props.message}</p>
+            <h3>{props.likes} likes</h3>
+        </div>
+    );
+};
+
+export default Tweet;
+
+// SECONDO STEP: dopo aver creato questo componente,
+// lo esporto in App.js.
+// L'export di un componente può essere di due tipi:
+// quello di default, come in questo caso, o quello
+// con le sole parentesi graffe.
+// Alternativa => export { Tweet };
+
+// QUINTO STEP: aggiungo le props come parametro della mia funzione.
+// Nell'elemento h3 sostituisco 'Author' (statico) con {props.name} (dinamico).
+// Lo stesso procedimento va seguito per il contenuto del tweet ed il numero di likes.
+// In p 'Tweet content' viene sostituito da {props.message}.
+// In h3 'Number of likes' viene sostituito da {props.likes} (+ 'likes').
