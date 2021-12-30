58cdc85188ad843dbd430933ae7517ac071c000e
Rebecca Mollica <rebecca.mollica@live.it>
2021-12-30 12:38:19 +0100

    destructuring props

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,11 +1,11 @@
 import styles from './Tweet.module.scss';
 
-function Tweet(props) {
+function Tweet({ name, message, likes }) {
     return (
         <div className={styles.tweet}>
-            <h3>{props.name}</h3>
-            <p>{props.message}</p>
-            <h3>{props.likes} likes</h3>
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>{likes} likes</h3>
         </div>
     );
 };
@@ -24,3 +24,17 @@ export default Tweet;
 // Lo stesso procedimento va seguito per il contenuto del tweet ed il numero di likes.
 // In p 'Tweet content' viene sostituito da {props.message}.
 // In h3 'Number of likes' viene sostituito da {props.likes} (+ 'likes').
+
+// function Tweet(props) {
+//     return (
+//         <div className={styles.tweet}>
+//             <h3>{props.name}</h3>
+//             <p>{props.message}</p>
+//             <h3>{props.likes} likes</h3>
+//         </div>
+//     );
+// };
+
+// QUINTO STEP ALTERNATIVO:
+// Invece di riportare ogni singola props, posso semplicemente passare come 
+// parametri tra parentesi graffe {name, message, likes} (props destrutturate).
